774c4a421bf7b3e442f0091c8c9a574b7ab69711
Logan <58321687+Smyli3@users.noreply.github.com>
2023-01-07 11:53:17 -0800

    Finished project #2

--- Project 2/main.py
+++ Project 2/main.py
@@ -1,8 +1,10 @@
+#Creates a document that contains the top 30 stories on HackerNews
 from bs4 import BeautifulSoup 
 import requests
+from datetime import date
 
+#Scrapes the data from the site and helps to format it
 def scrapper(url):
-    
     print('Extracting Hacker News Stories...')
     cnt = ''
     cnt +=('HN Top Stories:\n'+'-'*150+'\n')
@@ -10,18 +12,21 @@ def scrapper(url):
     content = response.content
     soup = BeautifulSoup(content,'html.parser')
     for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
-        cnt += (str(i+1)+'\t'+ tag.a.get('href') + '"\t' + tag.text + "\n")
+        cnt += (str(i+1)+'\t\t' + tag.text + "\n"+ tag.a.get('href') + "\n")
     return(cnt)
 
-cnt = scrapper('https://news.ycombinator.com/')
-
-
-print(cnt)
-'''def writefile(HTML):
-    pass
-
+#Writes the data to a txt
+def writefile(cnt):
+    f = open("NewsCollection/" + str(date.today()) + ".txt", "x")
+    f.close
+    f = open("NewsCollection/" + str(date.today()) + ".txt", "w")
+    f.write(cnt)
+    f.close
+    
+#Sets it off
 def main():
-    pass
+    cnt = scrapper('https://news.ycombinator.com/')
+    writefile(cnt)
     
-    
-main()'''
+#Begins program
+main()
